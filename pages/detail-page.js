5e4e4ecf5c4974259371b71434477a390b88eec6
Camille <cvanderharten@deloitte.nl>
2019-05-28 14:49:37 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,20 @@
 function submitComment() {
-        console.log('Hello!')
+    // gather data
+    const inputFieldName = document.getElementById('name')
+    const name = inputFieldName.value
+    const inputFieldMsg = document.getElementById('msg')
+    const msg = inputFieldMsg.value
+    
+    // create elements
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+
+    h3.innerHTML = '${name} said:'
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+
+    console.log(h3, p)
     }
