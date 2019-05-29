bdb27549a2bdbaa828b08d085909bfe8fd549033
Camille <cvanderharten@deloitte.nl>
2019-05-29 13:49:28 +0200

    set-up validation for form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,11 +1,57 @@
+function doesNotPassValidation(name, msg) {
+    if (!name && msg) 
+        {
+        alert('You forgot to fill in your name!')
+        return true;
+        }
+    if (name && !msg) 
+        {
+        alert('You forgot to fill in your message!')
+        return true;
+        }
+    if (!name && !msg) 
+        {
+        alert('You forgot to fill in your name and message!')
+        return true;
+        }
+    if (msg.length > 280) {
+            alert('Your comment is too long')
+        return true
+        }
+        return false
+}
+
+
+function nameToCapitalIfLower(string) {
+       return string.replace(string.charAt(0), string.charAt(0).toUpperCase())
+}
+
+
+function badWordCatcher(msg) {
+    if(Boolean(msg.match(/badword|Willem/gi))) {
+    alert('Your language is inappropriate')
+    return true
+    }
+    }
+        
+
 function submitComment() {
     // gather data
     const inputFieldName = document.getElementById('name')
-    const name = inputFieldName.value
+    const name = nameToCapitalIfLower(inputFieldName.value)                
     const inputFieldMsg = document.getElementById('msg')
     const msg = inputFieldMsg.value
+
+    // Validations
+if(doesNotPassValidation(name, msg)){
+       return null
+       }
     
-    // create comment elements
+if(badWordCatcher(msg)){
+    return null
+}
+                
+     // create comment elements
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
@@ -24,4 +70,5 @@ function submitComment() {
     // clear input fields
     inputFieldName.value = null
     inputFieldMsg.value = null
-    }
+
+}
